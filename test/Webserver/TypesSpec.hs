8f206c38fe9b9d4cf80ae5ca04ddc411124373c0
Kris Jenkins <krisajenkins@gmail.com>
2018-07-03 09:36:15 +0100

    Better error messages on the JSON decoding tests.

--- test/Webserver/TypesSpec.hs
+++ test/Webserver/TypesSpec.hs
@@ -7,16 +7,26 @@ import Compilation (files, mainFilename)
 import Control.Lens ((^..), (^?), _Right, asIndex, ifolded)
 import Data.Aeson (eitherDecode')
 import qualified Data.ByteString.Lazy as LBS
-import Test.Hspec (Spec, describe, it, shouldBe)
+import Data.Either (isRight)
+import Test.Hspec (Spec, describe, it, shouldBe, shouldSatisfy)
 import Webserver.Types (RPCCall, instructions)
 
 spec :: Spec
 spec =
-  describe "JSON" $
-  it "JSON decoding" $ do
-    decoded :: Either String RPCCall <-
-      eitherDecode' <$> LBS.readFile "test/Webserver/Sol2IELEAsm1.json"
-    decoded ^? (_Right . instructions . mainFilename) `shouldBe`
-      Just "mortal.sol"
-    decoded ^.. (_Right . instructions . files . ifolded . asIndex) `shouldBe`
-      ["mortal.sol", "owned.sol"]
+  describe "JSON" $ do
+    it "JSON decoding 1" $ do
+      decoded :: Either String RPCCall <-
+        eitherDecode' <$> LBS.readFile "test/Webserver/Sol2IELEAsm1.json"
+      decoded `shouldSatisfy` isRight
+      decoded ^? (_Right . instructions . mainFilename) `shouldBe`
+        Just "mortal.sol"
+      decoded ^.. (_Right . instructions . files . ifolded . asIndex) `shouldBe`
+        ["mortal.sol", "owned.sol"]
+    it "JSON decoding 2" $ do
+      decoded :: Either String RPCCall <-
+        eitherDecode' <$> LBS.readFile "test/Webserver/Sol2IELEAsm2.json"
+      decoded `shouldSatisfy` isRight
+      decoded ^? (_Right . instructions . mainFilename) `shouldBe`
+        Just "browser/ballot.sol"
+      decoded ^.. (_Right . instructions . files . ifolded . asIndex) `shouldBe`
+        ["browser/ballot.sol"]
