518e2f26e6eea2c618cf81367e5634fa55437660
Kris Jenkins <krisajenkins@gmail.com>
2018-09-17 13:11:23 +0100

    Improving our hindent setup for servant, and reformatting.

--- src/Webserver/API.hs
+++ src/Webserver/API.hs
@@ -1,24 +1,18 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DataKinds #-}
 
--- | hindent doesn't really play well with Servant's type operators, so
--- | just convenience we put the API spec in its own module, where we
--- | can avoid autoformatting.
 module Webserver.API
   ( API
   , Web
   ) where
 
 import Data.Text (Text)
-import Servant (Raw, Post, ReqBody, (:<|>), (:>), Get, JSON, PlainText)
-import Webserver.Types (RPCResponse, RPCRequest)
+import Servant ((:<|>), (:>), Get, JSON, PlainText, Post, Raw, ReqBody)
+import Webserver.Types (RPCRequest, RPCResponse)
 
-type Web =
-  "version" :> Get '[PlainText, JSON] Text
-  :<|>
-  "api" :> API
-  :<|>
-  Raw
+type Web
+   = "version" :> Get '[ PlainText, JSON] Text
+     :<|> "api" :> API
+     :<|> Raw
 
-type API =
-  ReqBody '[JSON] RPCRequest :> Post '[JSON] RPCResponse
+type API = ReqBody '[ JSON] RPCRequest :> Post '[ JSON] RPCResponse
