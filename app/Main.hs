aa5dfb5efd7f3d3f42d50b77fa24e954d43b7ecb
David Smith <shmish111+github@gmail.com>
2020-12-14 13:53:02 +0000

    remove riemann

--- app/Main.hs
+++ app/Main.hs
@@ -5,18 +5,10 @@
 
 module Main where
 
-import Control.Concurrent (forkIO, threadDelay)
-import Control.Monad (void, forever)
-import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Logger (MonadLogger, logInfoN, runStderrLoggingT)
-import Data.Function ((&))
 import Data.Monoid ((<>))
 import qualified Data.Text as Text
 import Development.GitRev (gitHash)
-import Network (HostName)
-import qualified Network.Monitoring.Riemann.Client as Riemann
-import qualified Network.Monitoring.Riemann.Event as Riemann
-import qualified Network.Monitoring.Riemann.TCPClient as Riemann
 import Network.Wai.Handler.Warp
   ( HostPreference
   , defaultSettings
@@ -44,14 +36,12 @@ import Options.Applicative
   , strOption
   , value
   )
-import qualified Riemann
 import UnliftIO (MonadUnliftIO)
 import qualified Webserver
 
 data Command = Run
   { _host :: HostPreference
   , _port :: Int
-  , _riemannHost :: HostName
   , _staticDir :: FilePath
   } deriving (Show, Eq)
 
@@ -70,10 +60,6 @@ commandParser = do
       auto
       (short 'p' <> long "port" <> help "Webserver port number" <> showDefault <>
        value 8080)
-  _riemannHost <-
-    strOption
-      (short 'r' <> long "riemann" <> help "Reimann host" <> showDefault <>
-       value "localhost")
   _staticDir <-
     argument str (metavar "STATIC_DIR" <> help "Static directory to serve up")
   pure Run {..}
@@ -81,16 +67,7 @@ commandParser = do
 runCommand :: (MonadUnliftIO m, MonadLogger m) => Command -> m ()
 runCommand Run {..} = do
   logInfoN . Text.pack $ "Running on " <> show _host <> ":" <> show _port
-  riemannClient <- liftIO $ Riemann.tcpClient _riemannHost 5555
-  Riemann.sendEvent riemannClient $
-    Riemann.ok Riemann.service & Riemann.version & Riemann.description "Startup"
-  let healthDelaySeconds = 30
-      healthDelayMicros = healthDelaySeconds * 1000 * 1000
-  void . liftIO . forkIO . forever $ do
-    threadDelay healthDelayMicros
-    Riemann.sendEvent riemannClient $
-      Riemann.ok (Riemann.service <> " health") & Riemann.version & Riemann.ttl (fromIntegral healthDelaySeconds * 2)
-  Webserver.run settings riemannClient _staticDir
+  Webserver.run settings _staticDir
   where
     settings = setHost _host . setPort _port $ defaultSettings
 
